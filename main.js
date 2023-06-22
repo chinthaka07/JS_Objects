f2d2a3ab0a8e1849d307b77680efb1323497a966
chinthaka <chinthakaweerasekara9@gmail.com>
2023-06-22 20:46:56 +0530

    1

---
+++ main.js
@@ -0,0 +1,22 @@
+let ranil={
+    name:'ranil',
+    address:'Colombo',
+    age:80,
+    horaKanawa(){
+        console.log(this.name,'Central Bank ekanm maru!')
+    }
+}
+
+let mahinda={
+    name:'mahinda',
+    address:'Matara',
+    age:85,
+    horaKanawa(){
+        console.log(this.name,'tsunami ekanm maru!')
+    }
+}
+console.log(ranil);
+console.log(mahinda);
+ranil.horaKanawa();
+ranil.name='wikkama singhe';
+ranil.horaKanawa();
