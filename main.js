1cb63b8fb94d8d485486ec9daa47ece0271c2ed4
chinthaka <chinthakaweerasekara9@gmail.com>
2023-06-22 21:30:12 +0530

    3

--- main.js
+++ main.js
@@ -1,26 +1,57 @@
-let ranil={
+class Person{
+    constructor(name,address,age){
+        this.name=name,
+            this.address=address,
+            this.age=age
+    }
+    getVote(){
+        console.log(this.name,'mn dinna');
+        return this;
+    }
+    horaKanawa(talent){
+        console.log(this.name,talent);
+        return this;
+    }
+    doProtest(){
+        console.log('#GoHome',this.name);
+
+    }
+}
+
+/*let ranil={
     name:'ranil',
     address:'Colombo',
     age:80,
     horaKanawa(){
-        console.log(this.name,'Central Bank ekanm maru!')
+        console.log(this.name,'Central Bank ekanm maru!');
     }
-}
-
-let mahinda={
+}*/
+let ranil=new Person('ranil','colombo',80);
+/*let mahinda={
     name:'mahinda',
     address:'Matara',
     age:85,
     horaKanawa(){
-        console.log(this.name,'tsunami ekanm maru!')
+        console.log(this.name,'tsunami ekanm maru!');
     }
-}
+}*/
+let mahinda=new Person('mahinda','Matara',85);
+/*let gota={
+    name:'Gota',
+    address:'USA',
+    age:75,
+    horaKanawa(){
+        console.log(this.name,'Rata Kawe!**');
+    }
+}*/
+let gota=new Person('Gota','USA',75);
+
 console.log(ranil);
 console.log(mahinda);
-ranil.horaKanawa();
-ranil.name='wikkama singhe';
-ranil.horaKanawa();
+console.log(gota);
+//=================
+ranil.getVote().horaKanawa('central banke eka nm maru').doProtest();
 
-mahinda['name']='**paksha';//dynamic
-mahinda.horaKanawa();
 
+//mahinda.horaKanawa('tsunami eka maru!');
+//gota.horaKanawa('rata kewa!');
